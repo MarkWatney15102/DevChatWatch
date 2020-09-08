b391e1e4b0614cf41f88d8bc2c68e32fe02e3cee
MarkWatney15102 <42077358+MarkWatney15102@users.noreply.github.com>
2020-09-08 17:37:58 +0200

    CHANGE: Version

--- src/DevChatWatch/DevChatWatchConfig.java
+++ src/DevChatWatch/DevChatWatchConfig.java
@@ -3,6 +3,6 @@ package DevChatWatch;
 public class DevChatWatchConfig 
 {
 	public static final String PLUGIN_NAME = "DevChatWatch";
-	public static final String PLUGIN_VERSION = "0.0.1";
+	public static final String PLUGIN_VERSION = "1.0.0";
 	public static final String PLUGIN_PATH = "plugins/" + PLUGIN_NAME;
 }
